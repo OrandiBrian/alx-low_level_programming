21fd06daee9686d08df25f680f781ad0fdfc5d55
Brian Orandi <brianorandi97@gmail.com>
2023-10-12 17:02:00 +0300

    isuuper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,4 +1,20 @@
-#include <stdio.h>
+#include <main.h>
+
+/**
+ * _isupper - printing uppercase letters
+ * 
+ * Return 0 or 1
+ */
+
+int _isupper(int c)
+{
+	if(c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	else
+		return (0);
+	}
+
 
 /**
  * main - Function that checks for uppercase character.
