3c43b72321b79796b2d9365ab6ee298129018379
Brian Orandi <brianorandi97@gmail.com>
2023-10-13 18:02:42 +0300

    largest number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -11,13 +11,10 @@
 int largest_number(int a, int b, int c)
 {
 	int largest;
+
 	if (a > b && a > c)
 	{
 		largest = a;
-	}
-	else if (a > b && c > a)
-	{
-		largest = c;
 	}
 	else if (b > c)
 	{
@@ -27,5 +24,6 @@ int largest_number(int a, int b, int c)
 	{
 		largest = c;
 	}
+
 	return (largest);
 }
