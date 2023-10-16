241dbaa521cf098d47e9f843f3346ccb36095997
Brian Orandi <brianorandi97@gmail.com>
2023-10-16 16:53:30 +0300

    Print string

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _puts - Prints a string followed by new line
+ * _puts - prints a string, followed by a new line, to stdout
  * @str: string to print
  */
 
@@ -12,5 +12,5 @@ void _puts(char *str)
 		_putchar(*str++);
 	}
 
-	_putchar("\n");
+	_putchar('\n');
 }
