715bc2c5df2f8860767f15509661bda4736079ac
Brian Orandi <brianorandi97@gmail.com>
2023-10-16 16:57:32 +0300

    Reverse string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_rev - prints string in reverse
+ * @s: string
+ * return: 0
+ */
+
+void print_rev(char *s)
+{
+	int lens = 0;
+	int o;
+
+	while (*s != '\0')
+	{
+		lens++;
+		s++;
+	}
+
+	s--;
+	
+	for (o = lens; o > 0; o--)
+	{
+		_putchar(*s);
+		s--;
+	}
+
+	_putchar('\n');
+}
